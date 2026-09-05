a2456529f5ae18a13af23e40506a482d86abd387
Anuja kumari <anujakumari781@gmail.com>
2026-09-05 22:47:14 +0530

    Add files via upload

---
+++ 08lec.cpp
@@ -0,0 +1,12 @@
+//CONSTANT VARIABLES
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    const int rate=23;
+    int b=9;
+    //YOU CAN CHANGE THE VALUE OF B BUT NOT A  BECAUSE A IS CONSTANT VARIABEL NOW ;
+
+    return 0;
+}
